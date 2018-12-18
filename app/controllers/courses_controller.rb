d1b34baa88488a1403785aa0a1741f871ff2bdf6
parasKumarSahu <2016csb1047@iitrpr.ac.in>
2018-12-19 00:09:46 +0530

    Fix for invalid query

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -7,7 +7,7 @@ class CoursesController < ApplicationController
 
     limit = params[:limit].to_i
     limit = rand(5..25) if limit.zero?
-
+    return nil if limit < 0
     @countries_to_display = all_countries.sample(limit)
     @countries_to_display.sort! { |a, b|  a["name"] <=> b["name"] }
   end
